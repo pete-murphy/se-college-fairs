cf3923d3e489c594a384d883fae95c4e9586fd72
Peter Murphy <peter.francis.murphy@gmail.com>
2018-10-04 17:35:30 -0400

    Fixed sorting, still need to validate some data

--- src/components/List.js
+++ src/components/List.js
@@ -1,29 +1,12 @@
 import React, { Fragment } from "react"
 import Item from "../containers/Item"
-import {
-  reduce,
-  either,
-  comparator,
-  gt,
-  prop,
-  map,
-  sort,
-  sortBy
-} from "ramda"
-
-const firstTruthy = ([head, ...tail]) =>
-  reduce(either, head, tail)
-const makeComparator = propName =>
-  comparator((a, b) =>
-    gt(prop(propName, a), prop(propName, b))
-  )
-const sortByProps = (props, list) =>
-  sort(firstTruthy(map(makeComparator, props)), list)
+import { sortBy } from "ramda"
+import parse from "date-fns/parse"
 
 export default ({ data, visibilityFilter }) => (
   <ul>
     {visibilityFilter(
-      sortBy(({ date: { parsed } }) => parsed)(data)
+      sortBy(({ date: { parsed } }) => parse(parsed), data)
     ).map(datum => (
       <Item key={`${datum.key}-I`} datum={datum} />
     ))}
--- src/components/SelectDate.js
+++ src/components/SelectDate.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react"
 import { Label } from "@atlaskit/field-base"
 import { DatePicker } from "@atlaskit/datetime-picker"
-import { format } from "date-fns"
+import format from "date-fns/format"
 
 export default ({ handleChange = console.log, label }) => (
   <Fragment>
